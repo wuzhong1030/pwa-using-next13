23870347559e384898ebfb1724919d5968a2493f
stone.wu <stone.wu@webeye.com>
2024-08-02 15:17:02 +0800

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "PWA with Next 13",
+  description: "PWA application with Next 13",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  // authors: [
+  //   { name: "Rajesh Prajapati" },
+  //   {
+  //     name: "Rajesh Prajapati",
+  //     url: "https://www.linkedin.com/in/raazeshp96/",
+  //   },
+  // ],
+  viewport: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
