710cc228d95aafa80fe4e0b3420516efeb901d5d
stone.wu <stone.wu@webeye.com>
2024-08-02 15:20:40 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex items-center justify-center text-8xl">
-        PWA with nextjs
+        PWA with nextjs two
       </div>
-      <Image
-        src={"/images/my-photo.jpg"}
-        width={500}
-        height={500}
-        alt="Picture of the author"
-      />
     </main>
   );
 }
